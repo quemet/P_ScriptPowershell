65859684a327194ca36530dcfd57c06f11a89a43
quemet <129742514+quemet@users.noreply.github.com>
2024-01-12 11:34:40 +0100

    Add files via upload

---
+++ createDirectory.ps1
@@ -0,0 +1,5 @@
+$path = "E:\Entreprise";
+
+New-Item -Path $path -Name "Commun" -ItemType Directory;
+New-Item -Path $path -Name "Groupes" -ItemType Directory;
+New-Item -Path $path -Name "Utilisateurs" -ItemType Directory;
